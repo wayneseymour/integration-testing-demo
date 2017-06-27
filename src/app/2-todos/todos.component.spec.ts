e5eff77cf6b72ae160f7d7ba80c4087db4a97657
Wayne Kenneth Seymour III <WayneSeymour@iheartmedia.com>
2017-06-27 12:30:54 -0400

    Done with the vid, but a test is failing.  Not good.

--- src/app/2-todos/todos.component.spec.ts
+++ src/app/2-todos/todos.component.spec.ts
@@ -5,7 +5,7 @@ import { DebugElement } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service';
-
+import { Observable } from 'rxjs';
 //NOTE: I've deliberately excluded this suite from running
 // because the test will fail. This is because we have not
 // provided the TodoService as a dependency to TodosComponent.
@@ -29,10 +29,16 @@ describe('TodosComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TodosComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should load todos from the server', () => {
+    const service = TestBed.get(TodoService); // only works if the dep is defined at the module level
+    // const service = fixture.debugElement.injector.get(TodoService);
+    spyOn(service, 'getTodos').and.returnValue(Observable.from([1, 2, 3]));
+    fixture.detectChanges();
+    expect(component.todos.length).toBe(3);
   });
 });
